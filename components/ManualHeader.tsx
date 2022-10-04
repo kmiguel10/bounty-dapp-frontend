2f8cdad639903419ab85aa06430abe2d87cc6d88
Kent Miguel <kentmiguel10@gmail.com>
2022-10-04 09:14:41 -0400

    Added connect button
    A robust connect button
    - the application won't insist on asking to connect if disconnected
    - on refresh the dapp recognizes that an account is already connected and will keep connected

---
+++ components/ManualHeader.tsx
@@ -0,0 +1,51 @@
+import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
+
+const ManualHeader = () => {
+    const { enableWeb3, isWeb3Enabled, account, Moralis, deactivateWeb3 } = useMoralis()
+
+    useEffect(() => {
+        if (isWeb3Enabled) return //if connected already then return
+        if (typeof window !== "undefined") {
+            if (window.localStorage.getItem("connected")) {
+                enableWeb3()
+            }
+        }
+        //enableWeb3()
+    }, [isWeb3Enabled])
+
+    //create another useEffect to check if we disconnected
+    useEffect(() => {
+        Moralis.onAccountChanged((account) => {
+            console.log(`Account changed to ${account}`)
+            if (account == null) {
+                window.localStorage.removeItem("connected")
+                deactivateWeb3()
+                console.log("Null account found")
+            }
+        })
+    }, [])
+
+    return (
+        <div>
+            {account ? (
+                <div>
+                    Connected to {account.slice(0, 6)}...{account.slice(account.length - 4)}
+                </div>
+            ) : (
+                <button
+                    onClick={async () => {
+                        await enableWeb3() //asks to connect
+                        if (typeof window !== "undefined") {
+                            window.localStorage.setItem("connected", "injected")
+                        }
+                    }}
+                >
+                    Connect
+                </button>
+            )}
+        </div>
+    )
+}
+
+export default ManualHeader
