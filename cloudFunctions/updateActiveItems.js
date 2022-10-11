336257d744bc7e8577785181db438fe65a4bdc29
Kent Miguel <kentmiguel10@gmail.com>
2022-10-11 16:08:30 -0400

    Created cloud function for ClaimBounty
    Deletes claimed bounty from ActiveItem table

--- cloudFunctions/updateActiveItems.js
+++ cloudFunctions/updateActiveItems.js
@@ -2,8 +2,6 @@
 // Add items when they are posted
 // Remove the, when they are claimed
 
-//const { default: Moralis } = require("moralis-v1")
-
 //everytime a new bounty is created, run a function -- which is to add to the ActiveItem list
 Moralis.Cloud.afterSave("CreateBounty", async (request) => {
     //Every event gets triggered twice, once on unconfirmed, and again on confirmed
@@ -27,5 +25,43 @@ Moralis.Cloud.afterSave("CreateBounty", async (request) => {
         )
         logger.info("Saving. . . ")
         await activeItem.save()
+    }
+})
+
+//Everytime a bounty is claimed, remove that item from ActiveItem table
+Moralis.Cloud.afterSave("ClaimBounty", async (request) => {
+    const confirmed = request.object.get("confirmed")
+    const logger = Moralis.Cloud.getLogger()
+    logger.info("Looking for confirmed Tx . . .")
+
+    // event ClaimBounty(
+    //     uint256 indexed id,
+    //     address indexed owner,
+    //     string name,
+    //     uint256 price,
+    //     bool status
+    // );
+
+    if (confirmed) {
+        //get ActiveItem table
+        const ActiveItem = Moralis.Object.extend("ActiveItem")
+        const query = new Moralis.Query(ActiveItem)
+        query.equalTo("uid", request.object.get("uid"))
+        query.equalTo("price", request.object.get("price"))
+        query.equalTo("name", request.object.get("name"))
+        logger.info(`Bounty | Query: ${query}`)
+        const claimedBounty = await query.first()
+        logger.info(`Bounty | claimedBounty: ${JSON.stringify(claimedBounty)}`)
+        if (claimedBounty) {
+            logger.info(`Deleting claimed bounty ${claimedBounty.id}`)
+            await claimedBounty.destroy()
+            logger.info(
+                `Deleting claimedBounty ${request.object.get(
+                    "uid"
+                )} from ActiveItem table since it was claimed.`
+            )
+        }
+    } else {
+        logger.info(`No bounty with id: ${request.object.get("uid")}`)
     }
 })
